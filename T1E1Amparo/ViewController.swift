e47278f38d0b7f2797daf7fdee9e5a391532862f
ampaamg <manzano_amp@102.red-194-179-3.static.ccgg.telefonica.net>
2014-12-04 21:31:00 +0100

    Initial Commit

---
+++ T1E1Amparo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T1E1Amparo
+//
+//  Created by Amparo Manzano on 04/12/14.
+//  Copyright (c) 2014 Manzano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
